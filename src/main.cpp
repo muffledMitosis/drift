0d55c4aacebdf790821a348842dbd3b8da3557b8
Meth Munindradasa <methsndr@gmail.com>
2023-02-11 16:10:58 +0530

    Working info pin

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,20 @@ void setup()
 {
   Serial.begin(9600);
   Serial1.begin(9600, SERIAL_8N1, 4,5);
+
+  Serial1.println("AT");
+  delay(1000);
+  Serial1.println("AT+CGNSPWR=1");
+  delay(1000);
 }
 
 void loop()
 {
-  Serial1.println("LOL");
-  while(Serial1.available())
-  {
-    Serial.print(Serial1.read());
-  }
+  Serial1.println("AT+CGNSINF");
   delay(1000);
+  if(Serial1.available())
+  {
+    String res = Serial1.readString();
+    Serial.println(res);
+  }
 }
