577788701e720d1806e57ac7b5a0ed00539d7dac
Meth Munindradasa <methsndr@gmail.com>
2023-02-14 12:15:43 +0530

    Test GPS and BLE

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,55 @@
 #include <Arduino.h>
 
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+#include "./utils.h"
+
+#include "./SIM808.h"
+
+#define SERVICE_UUID          "6f14048f-e286-406a-bb2e-75eaf121fca8"
+#define CHARACTERISTIC_UUID   "b1b67348-22a8-490a-ad22-f841c0d8582b"
+
+BLEServer *bleServer;
+BLEService *bleService;
+BLECharacteristic *bleCharac;
+
+SIM808 *sim808;
+
 void setup()
 {
   Serial.begin(9600);
-  Serial1.begin(9600, SERIAL_8N1, 4,5);
+  Serial1.begin(9600, SERIAL_8N1, 4,5); // RX, TX
 
-  Serial1.println("AT");
-  delay(1000);
-  Serial1.println("AT+CGNSPWR=1");
-  delay(1000);
+  BLEDevice::init("Drift");
+
+  bleServer = BLEDevice::createServer();
+  bleService = bleServer->createService(SERVICE_UUID);
+  bleCharac = bleService->createCharacteristic(
+                            CHARACTERISTIC_UUID,
+                            BLECharacteristic::PROPERTY_READ |
+                            BLECharacteristic::PROPERTY_WRITE
+                          );
+
+  bleCharac->setValue("Drift says hello");
+  bleService->start();
+
+  BLEAdvertising *bleAdv = BLEDevice::getAdvertising();
+  bleAdv->addServiceUUID(SERVICE_UUID);
+  bleAdv->setScanResponse(true);
+  bleAdv->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  bleAdv->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+
+  sim808 = new SIM808(&Serial1);
+
+  sim808->init();
+  sim808->initGPS();
 }
 
 void loop()
 {
-  Serial1.println("AT+CGNSINF");
+  Serial.println(sim808->getGPSInfo());
   delay(1000);
-  if(Serial1.available())
-  {
-    String res = Serial1.readString();
-    Serial.println(res);
-  }
 }
